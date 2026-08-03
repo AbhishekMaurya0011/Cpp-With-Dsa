997697f6bbab089b34a78d0dc836f7d665c80aab
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-08-03 22:34:05 +0530

    Add files via upload

---
+++ Pattern of cpp/trianglepattern1.cpp
@@ -0,0 +1,23 @@
+// print triangle pattern using for loop
+//  for n=4
+//  *
+//  * *
+//  * * *
+//  * * * *
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
