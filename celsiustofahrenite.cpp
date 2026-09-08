72cb67ea3dab744a33a1796a9150e9cff2a90164
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-08 21:53:03 +0530

    Add files via upload

---
+++ celsiustofahrenite.cpp
@@ -0,0 +1,16 @@
+// WAP to convert the temperature celsius into fagrenite by using the functions
+#include <iostream>
+using namespace std;
+float fahrenhite(float celsius)
+{
+    return (celsius * 9 / 5) + 32;
+}
+int main()
+{
+    float c;
+    cout << "Enter the Temperature in celsius: ";
+    cin >> c;
+
+    cout << "The temperature in Fahrenite:" << fahrenhite(c) << endl;
+    return 0;
+}
