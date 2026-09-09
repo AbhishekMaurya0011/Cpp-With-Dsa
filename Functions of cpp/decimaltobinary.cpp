feae4e30ed1660c331c67971726d9d5843596a8f
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-09 10:15:04 +0530

    Add files via upload

---
+++ Functions of cpp/decimaltobinary.cpp
@@ -0,0 +1,28 @@
+// WAP to convert the decimal into the binary number by using the function.
+#include <iostream>
+using namespace std;
+void binary(int n)
+{
+    int binary[32], i = 0;
+
+    while (n > 0)
+    {
+        binary[i] = n % 2;
+        n /= 2;
+        i++;
+    }
+    for (i = i - 1; i >= 0; i--)
+        cout << binary[i];
+}
+
+int main()
+{
+    int n;
+    cout << "Enter Decimal Number:";
+    cin >> n;
+
+    cout << "The Binary Number is:";
+    binary(n);
+
+    return 0;
+}
