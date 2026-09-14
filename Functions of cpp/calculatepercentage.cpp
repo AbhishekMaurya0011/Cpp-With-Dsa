46898b9fc84ff8111644ec570c81fc70c6472539
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-14 23:10:24 +0530

    Add files via upload

--- Functions of cpp/calculatepercentage.cpp
+++ Functions of cpp/calculatepercentage.cpp
@@ -1,18 +1,20 @@
-// WAP to caluclate the percentage of a student marks using the functions and input marks by the user.
+// WAP to calculate the percentage of obtained and total value given by the user.
 #include <iostream>
 using namespace std;
-float percentage(float total, float obtained)
+float percentage(float obtained, float total)
 {
     return (obtained / total) * 100;
 }
+
 int main()
 {
-    float total, obtained;
-    cout << "Enter The Total marks:";
-    cin >> total;
-    cout << "Enter Obtained marks:";
+    float obtained, total;
+    cout << "Enter Obtained :";
     cin >> obtained;
 
-    cout << "The percentage is: " << percentage(total, obtained) << "%";
+    cout << "Enter Total:";
+    cin >> total;
+
+    cout << "The Percentage is:" << percentage(obtained, total) << "%";
     return 0;
 }
