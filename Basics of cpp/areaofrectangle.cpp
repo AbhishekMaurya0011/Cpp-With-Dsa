e417ad791767ce17b35d4cfacfee616dff3f72ee
AbhishekMaurya0011 <abhishekmaurya9569831340@gmail.com>
2026-09-14 23:08:47 +0530

    Add files via upload

---
+++ Basics of cpp/areaofrectangle.cpp
@@ -0,0 +1,15 @@
+// WAP to calulate the area of rectangle.
+#include <iostream>
+using namespace std;
+int main()
+{
+    float length, width;
+    cout << "Enter Length Of Rectangle:";
+    cin >> length;
+
+    cout << "Enter Width Of Rectangle:";
+    cin >> width;
+
+    cout << "The Area Of Reactangle is:" << length * width << " square units";
+    return 0;
+}
